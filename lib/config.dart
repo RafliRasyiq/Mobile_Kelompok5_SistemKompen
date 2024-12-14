89855046230cb3a0475ad5355ccb814d2c867be9
rikatsetya <rikatsetyagus@gmail.com>
2024-12-14 19:28:56 +0700

    "Fitur Update Kompen Selesai"

--- lib/config.dart
+++ lib/config.dart
@@ -1,6 +1,6 @@
 class Config {
   //URL dasar
-  static const String base_domain = 'http://192.168.1.5:8000';
+  static const String base_domain = 'http://192.168.1.3:8000';
 
   //Endpoint Login
   static const String login_endpoint = '$base_domain/api/login';
@@ -9,16 +9,25 @@ class Config {
   static const String mahasiswa_endpoint = '$base_domain/api/mahasiswa/mahasiswa_data';
   static const String mahasiswa_update_profile_endpoint = '$base_domain/api/mahasiswa/edit_profile';
   static const String mahasiswa_update_password_endpoint = '$base_domain/api/mahasiswa/edit_password';
+  static const String mahasiswa_kompen_selesai_endpoint = '$base_domain/api/mahasiswa/show_kompen_selesai';
 
 
   static const String dosen_endpoint = '$base_domain/api/dosen/dosen_data';
   static const String dosen_update_profile_endpoint = '$base_domain/api/dosen/edit_profile';
   static const String dosen_update_password_endpoint = '$base_domain/api/dosen/edit_password';
+  static const String dosen_kompen_selesai_endpoint = '$base_domain/api/dosen/show_kompen_selesai';
+  static const String dosen_show_kompen_selesai_endpoint = '$base_domain/api/dosen/detail_kompen_selesai';
+  static const String dosen_update_kompen_selesai_endpoint = '$base_domain/api/dosen/update_kompen_selesai';
+
 
 
   static const String tendik_endpoint = '$base_domain/api/tendik/tendik_data';
   static const String tendik_update_profile_endpoint = '$base_domain/api/tendik/edit_profile';
   static const String tendik_update_password_endpoint = '$base_domain/api/tendik/edit_password';
+  static const String tendik_kompen_selesai_endpoint = '$base_domain/api/tendik/show_kompen_selesai';
+  static const String tendik_show_kompen_selesai_endpoint = '$base_domain/api/tendik/detail_kompen_selesai';
+  static const String tendik_update_kompen_selesai_endpoint = '$base_domain/api/tendik/update_kompen_selesai';
+
 
 
   static const String list_mhs_kompen_endpoint = '$base_domain/api/kompen/list_mhs_kompen';
